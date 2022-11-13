8c503f4223270d8048ff2061c737ff98888e9eba
渡邉尚輝 <watanabenaoki@MacBook-Air-2.local>
2022-11-14 02:13:47 +0900

    ドロワーコンポーネント作成の際のコメントを記述

--- src/pages/TwitterHome.tsx
+++ src/pages/TwitterHome.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { atom } from "recoil";
-
 import { useSetRecoilState } from "recoil";
 import TwitterDrawer from "../components/twitter/TwitterDrawer";
+
+//npm i recoil
+//npm install @headlessui/react
+//を追加する必要あり。
 
 export const isDrawerOpenState = atom({
   key: "isDrawerOpenState", // unique ID (with respect to other atoms/selectors)
   default: true, // default value (aka initial value)
 });
 
+//サイドバーとかでボタン押したら横からメニューがでてくるやつ
 const TwitterHome = () => {
   const setIsDrawerOpen = useSetRecoilState(isDrawerOpenState);
   return (
